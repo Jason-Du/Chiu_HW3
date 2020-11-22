2dd780a7ba7d5b18cc5debc97fdf67c61c9bf57c
“Jason-Duh(Home) <dushiun@gmail.com>
2020-11-22 17:53:59 +0800

    C file upload and delete redundant code

--- sim/prog2/main.c
+++ sim/prog2/main.c
@@ -3,9 +3,16 @@ int main(void)
 	extern short _binary_image_bmp_start;
 	extern short _binary_image_bmp_end;
 	extern short _test_start;
-	for(int i=0;&_binary_image_bmp_start<&_binary_image_bmp_end;++i)
+	for(int i=0;(&_binary_image_bmp_start+i)<&_binary_image_bmp_end;++i)
 	{
-		*(&_test_start+i)=*(&_binary_image_bmp_end+i)*0.11+*(&_binary_image_bmp_end+i+1)*0.59+*(&_binary_image_bmp_end+i+2)*0.3;
+		if(i<54)
+		{
+			*(&_test_start+i)=*(&_binary_image_bmp_end+i);
+		}
+		else
+		{
+			*(&_test_start+i)=*(&_binary_image_bmp_end+i)*0.11+*(&_binary_image_bmp_end+i+1)*0.59+*(&_binary_image_bmp_end+i+2)*0.3;
+		}
 	}
 	return 0;
 }
