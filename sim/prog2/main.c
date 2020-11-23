d51557d27cefc1cc10e067e536cb0669276d4503
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-11-23 15:41:23 +0800

    all done

--- sim/prog2/main.c
+++ sim/prog2/main.c
@@ -1,4 +1,4 @@
-/*
+
 int main(void)
 {
 
@@ -11,9 +11,7 @@ int main(void)
 	{
 		*(&_test_start+i)=*(&_binary_image_bmp_start+i);
 	}
-	
-	
-	for(int i=54;i<_binary_image_bmp_size;i=i+3)
+	for(int i=54;i<&_binary_image_bmp_size;i=i+3)
 	{
 		*((&_test_start)+i)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
 		*((&_test_start)+i+1)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
@@ -23,28 +21,5 @@ int main(void)
 	return 0;
 
 	
-}
-*/
-#include <stdio.h>
-int main(void)
-{
-	extern char _binary_image_bmp_start;
-	extern int _binary_image_bmp_size;
-	
-
-	extern char _test_start;
-	for(int i=0;i<54;i++)
-	{
-		*(&_test_start+i)=*(&_binary_image_bmp_start+i);
-	}
-	for(int i=54;i<&_binary_image_bmp_size;i=i+3)	{
-		*( (&_test_start) +i)  =( *(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59
-						+*(&_binary_image_bmp_start+i+2)*30 )/100;
-		*( (&_test_start) +i+1)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
-		*( (&_test_start) +i+2)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
-	}
-	return 0; 
-	
-
 }
 
