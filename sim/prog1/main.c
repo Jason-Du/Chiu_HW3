5c8d9b21196e0003e1ffdb9fd6ca2187098d1e32
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-11-23 13:23:19 +0800

    first version of prog2

---
+++ sim/prog1/main.c
@@ -0,0 +1,37 @@
+
+int main(void)
+{
+	
+	extern  int  array_size;
+	extern short signed int array_addr;
+
+	extern short signed int _test_start;
+	int number=*(&(array_size));
+	
+	int i=0;
+	int j=0;
+	
+	short signed int temp =0;
+	
+	for (i=0; i <number ;++i)
+	{
+		for (j=0; j <number; ++j)
+		{
+			if (  (*(&(array_addr)+j) )  >  ( *(&(array_addr)+i) )  ) 
+			{
+				temp=*(&(array_addr)+j);
+				*(&(array_addr)+j)=*(&(array_addr)+i);
+				*(&(array_addr)+i)=temp;
+			}
+			
+		}
+	}
+
+	for (i=0; i <number; ++i)
+	{
+	
+		*(&(_test_start)+i)= *(&(array_addr)+i);
+		
+	}
+	return 0;
+}
--- sim/prog2/main.c
+++ sim/prog2/main.c
@@ -1,18 +1,30 @@
+
+void gray(char* image_start,char* image_end,char* store_address)
+{
+	for(int i=28;(&image_start+i)<&image_end;i=i+3)
+	{
+		*(&store_address+i)=*(&image_start+i)*0.11+*(&image_startt+i+1)*0.59+*(&image_start+i+2)*0.3;
+	}
+}
+*
 int main(void)
 {
-	extern short _binary_image_bmp_start;
-	extern short _binary_image_bmp_end;
-	extern short _test_start;
-	for(int i=0;(&_binary_image_bmp_start+i)<&_binary_image_bmp_end;++i)
+	extern char _binary_image_bmp_start;
+	extern char _binary_image_bmp_end;
+	extern char _test_start;
+	for(int i=0;i<=53;++i)
 	{
-		if(i<54)
-		{
-			*(&_test_start+i)=*(&_binary_image_bmp_end+i);
-		}
-		else
-		{
-			*(&_test_start+i)=*(&_binary_image_bmp_end+i)*0.11+*(&_binary_image_bmp_end+i+1)*0.59+*(&_binary_image_bmp_end+i+2)*0.3;
-		}
+		*(&_test_start+i)=*(&_binary_image_bmp_start+i);
 	}
+	
+	
+	for(int i=54;(&_binary_image_bmp_start+i)<&_binary_image_bmp_end;i=i+3)
+	{
+		*(&_test_start+i)=*(&_binary_image_bmp_start+i)*0.11+*(&_binary_image_bmp_start+i+1)*0.59+*(&_binary_image_bmp_start+i+2)*0.3;
+	}
+	
+	/*
+	gray(&_binary_image_bmp_start,&_binary_image_bmp_end,&_test_start);
+	*/
 	return 0;
 }
