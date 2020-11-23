9d67b32dd3400f90b522dd33f85896c4b5cb647d
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-11-23 14:30:00 +0800

    new  c code

--- sim/prog2/main.c
+++ sim/prog2/main.c
@@ -1,31 +1,47 @@
-
-void gray(char* image_start,char* image_end,char* store_address)
-{
-	for(int i=28;(&image_start+i)<&image_end;i=i+3)
-	{
-		*(&store_address+i)=*(&image_start+i)*0.11+*(&image_startt+i+1)*0.59+*(&image_start+i+2)*0.3;
-	}
-}
-*
+/*
 int main(void)
 {
+
 	extern char _binary_image_bmp_start;
-	extern char _binary_image_bmp_end;
+	extern int _binary_image_bmp_size;
+
 	extern char _test_start;
-	for(int i=0;i<=53;++i)
+
+	for(int i=0;i<54;i++)
 	{
 		*(&_test_start+i)=*(&_binary_image_bmp_start+i);
 	}
 	
 	
-	for(int i=54;(&_binary_image_bmp_start+i)<&_binary_image_bmp_end;i=i+3)
+	for(int i=54;i<_binary_image_bmp_size;i=i+3)
 	{
-		*(&_test_start+i)=*(&_binary_image_bmp_start+i)*0.11+*(&_binary_image_bmp_start+i+1)*0.59+*(&_binary_image_bmp_start+i+2)*0.3;
+		*((&_test_start)+i)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
+		*((&_test_start)+i+1)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
+		*((&_test_start)+i+2)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
 	}
-	
-	/*
-	gray(&_binary_image_bmp_start,&_binary_image_bmp_end,&_test_start);
-	*/
+
 	return 0;
+
 	
 }
+*/
+int main(void)
+{
+	extern char _binary_image_bmp_start;
+	extern int _binary_image_bmp_size;
+
+	extern char _test_start;
+	for(int i=0;i<54;i++)
+	{
+		*(&_test_start+i)=*(&_binary_image_bmp_start+i);
+	}
+	for(int i=54;i<_binary_image_bmp_size;i=i+3)	{
+		*( (&_test_start) +i)  =( *(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59
+						+*(&_binary_image_bmp_start+i+2)*30 )/100;
+		*( (&_test_start) +i+1)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
+		*( (&_test_start) +i+2)=(*(&_binary_image_bmp_start+i)*11+*(&_binary_image_bmp_start+i+1)*59+*(&_binary_image_bmp_start+i+2)*30)/100;
+	}
+	return 0; 
+
+}
+
